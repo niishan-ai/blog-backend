b3ec746213ca0ea5bb417fe73e3e13e43b2e4295
niishan-ai <niishanpaudel22@gmail.com>
2025-05-04 22:13:40 +0545

    Update server.js

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/api/blogs', blogRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
